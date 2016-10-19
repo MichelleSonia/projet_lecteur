9c485129378cffec6494d798b68d864d8ab73c80
MichelleSonia <s.guiadem@gmail.com>
2016-10-19 19:42:30 +0200

    youpi

--- main.cpp
+++ main.cpp
@@ -14,9 +14,31 @@ int main()
         {
             if (event.type == sf::Event::Closed)
                 window.close();
-            
-            //etc...
-        }
+             // evènements agissant sur clavier
+             // Mettre en pause ou en Lecture...
+            else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Pause)) 
+				window.setActive();
+            // se déplacer en haut...
+            else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) 
+				window.setActive();
+            // se déplacer à bas...
+            else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) 
+				window.setActive();
+            // se déplacer à gauche...
+            else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+				window.setActive();
+			// déplacer vers la droite... 
+			else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+				window.setActive();
+				
+			// evènements agissant sur la souris
+			else if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) 
+				window.setActive();
+			else if (sf::Mouse::isButtonPressed(sf::Mouse::Right)) 
+				window.setActive();
+			else if (sf::Mouse::isButtonPressed(sf::Mouse::Middle)) 
+				window.setActive();
+		}
 // a chaque tour de boucle on met à jour...
         window.clear(); //...effacer
         window.draw(); //...dessiner tous les objets
